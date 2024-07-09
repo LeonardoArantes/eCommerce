d888bcc48e5adbe5a32f4a22efc1e7a005330768
LeonardoArantes <leonardoarantes86@gmail.com>
2024-07-09 18:27:40 -0300

    Revert "Commit inicial do módulo de usuários"
    This reverts commit 7ee7548d21ddd382b4b54caabf6a616486c4f95d.

--- src/main/java/com/ecommerce/fiap/controller/exception/ControllerInsufficientBalanceException.java
+++
@@ -1,7 +0,0 @@
-package com.ecommerce.fiap.controller.exception;
-
-public class ControllerInsufficientBalanceException extends RuntimeException {
-    public ControllerInsufficientBalanceException(String message) {
-        super(message);
-    }
-}
